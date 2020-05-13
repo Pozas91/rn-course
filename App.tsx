aeab57603f019811e5aa2b22f87d935c755ffdf4
Pozas91 <pozas91@gmail.com>
2020-05-13 11:20:49 +0200

    Setting Up A New Project Planning the App Working with Core Components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <View>
+      <View>
+        ...
+      </View>
+      <View>
+        ...
+      </View>
+      {/*<Text>Open up App.tsx to start working on your app!</Text>*/}
     </View>
   );
 }
