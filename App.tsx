df045ebbe443d9269f793386d139543698eed56c
Pozas91 <pozas91@gmail.com>
2020-05-13 21:14:01 +0200

    Inline Styles & StyleSheet Objects

--- App.tsx
+++ App.tsx
@@ -1,48 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import {Button, StyleSheet, TextInput, View} from 'react-native';
 
 export default function App() {
   return (
-    <View
-      style={{
-        padding: 50,
-        flexDirection: 'row',
-        width: '80%',
-        height: 300,
-        justifyContent: 'space-around',
-        alignItems: 'stretch',
-      }}
-    >
-      <View
-        style={{
-          backgroundColor: 'red',
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>1</Text>
+    <View style={{padding: 50}}>
+      <View style={{flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+        <TextInput placeholder={'Course Goal'} style={{borderColor: 'black', borderWidth: 1, padding: 10, width: '80%'}}/>
+        <Button title={'ADD'} onPress={() => ''}/>
       </View>
-      <View
-        style={{
-          backgroundColor: 'blue',
-          flex: 2,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>2</Text>
-      </View>
-      <View
-        style={{
-          backgroundColor: 'green',
-          flex:3,
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}
-      >
-        <Text>3</Text>
+      <View>
+
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
